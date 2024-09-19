19659631dfd7c198ffe9a960e76c418d1db9f7c6
SenaiButzke <bruno.butzke@sc.senai.br>
2024-09-19 02:28:50 -0300

    fé que da certo

--- src/server.js
+++ src/server.js
@@ -169,3 +169,36 @@ module.exports = async (req, res) => {
 /* export default function handler(req, res) {
   res.status(200).send('hello world');
 } */
+
+
+
+
+
+
+
+  
+
+  /* const express = require('express');
+  const next = require('next');
+  
+  const dev = process.env.NODE_ENV !== 'production';
+  const app = next({ dev });
+  const handle = app.getRequestHandler();
+  
+  app.prepare().then(() => {
+    const server = express();
+  
+    server.get('/api/hello', (req, res) => {
+      res.json({ status: 200, message: 'hello world' });
+    });
+  
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+  
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  }); */
