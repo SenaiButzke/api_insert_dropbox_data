b321003fe77ee6976de6b93bd1c6fef759767f3e
SenaiButzke <bruno.butzke@sc.senai.br>
2024-09-30 14:45:47 -0300

    fix id name

--- src/server.js
+++ src/server.js
@@ -75,7 +75,7 @@ app.post('/alarm', async(req, res)=> {
             "effectValue",
           ) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
         [
-            pmeId,
+            alarmId,
             getId,
             source,
             timestamp,
